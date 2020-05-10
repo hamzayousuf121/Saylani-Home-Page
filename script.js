27e688cf56d196d22eda57ba73a1fac6b7d70b76
Muhammad Hamza <48496170+hamzayousuf121@users.noreply.github.com>
2020-05-10 15:05:39 +0500

    Update script.js
    Function updated

--- script.js
+++ script.js
@@ -9,13 +9,3 @@ $('.number').counterUp({
     delay: 10,
     time: 2000
 });
-
-function func() {
-    alert('test');
-    let demo = new CountUp('num', 94.62);
-    if (!demo.error) {
-        demo.start();
-    } else {
-        console.error(demo.error);
-    }
-}
